87fb922460d8de29aa9cf18889bbbe80dcb49d0c
Sergej <12serega34@mail.ru>
2022-01-05 15:55:09 +0300

    fixed comment styles

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,15 +21,18 @@ class HomeController extends Controller
         $post = Post::where('slug', $slug)->firstOrFail(); //firstOrFail будет получать первый результат запроса; однако, если результат не найден, будет выброшено исключение. Иначе - если при вводе адреса нужный файл в БД не найден (страницы не существует), клиенту будет отправлен HTTP-ответ 404
         $post->views ++;
         $post->update(); //обновляем, чтобы в базе увеличился счетчик просмотров
+
         $randomPost = Post::all()->random(2); //отправляем 2 случайные записи в представление - для блока случайных записей
+
         $comments = Comment::where('post_id', $post->id)->get();;
         $id = [];
         foreach($comments as $comment) {
             $id[] = $comment->value('user_id');
         }
-        $userName = DB::table('users')->where('id', $id)->value('name'); //Если не нужна вся строка, можете извлечь одно значение из записи с помощью метода value, который вернет значение столбца напрямую
 
-        return view('posts.article', compact('post', 'randomPost', 'comments', 'userName'));
+        $userNameForComments = DB::table('users')->where('id', $id)->value('name'); //Если не нужна вся строка, можете извлечь одно значение из записи с помощью метода value, который вернет значение столбца напрямую
+
+        return view('posts.article', compact('post', 'randomPost', 'comments', 'userNameForComments'));
     }
 
     public function blog()
--- resources/views/posts/article.blade.php
+++ resources/views/posts/article.blade.php
@@ -85,12 +85,12 @@
                                 <div class="col-lg-12">
                                     <div class="comments-list">
                                         @foreach($comments as $comment)
-                                        <div class="media">
+                                        <div class="media @if($loop->last) last-child @endif">
                                             <a class="media-left" href="#">
                                                 <img src="upload/author.jpg" alt="" class="rounded-circle">
                                             </a>
                                             <div class="media-body">
-                                                <h4 class="media-heading user_name">{{ $userName }}<small>5 days ago</small></h4>
+                                                <h4 class="media-heading user_name">{{ $userNameForComments }}<small>{{ $comment->formattingTheCreationDate($comment->created_at) }}</small></h4>
                                                 <p>{{ $comment->content }}</p>
                                                 <a href="#" class="btn btn-primary btn-sm">Reply</a>
                                             </div>
